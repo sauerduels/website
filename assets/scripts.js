bb7ecb64a1928906a735d3ec6611afb54b004533
AngrySnout <splintersal@gmail.com>
2018-01-01 21:55:17 +0200

    Add tabbed interface to rankings page

--- assets/scripts.js
+++ assets/scripts.js
@@ -7,3 +7,19 @@ window.toggleElement = function(id) {
   }
   return false;
 }
+
+window.switchTab = function(id) {
+  var elems = document.getElementsByClassName('tab-content');
+  for (var i = 0; i < elems.length; i++) {
+    if (elems[i].id === id) elems[i].classList.add('active');
+    else elems[i].classList.remove('active');
+  }
+  
+  elems = document.getElementsByClassName('tab');
+  for (var i = 0; i < elems.length; i++) {
+    if (elems[i].id === 'tab-'+id) elems[i].classList.add('active');
+    else elems[i].classList.remove('active');
+  }
+
+  return false;
+}
